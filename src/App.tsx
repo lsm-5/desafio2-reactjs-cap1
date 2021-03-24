6a00abe7fb1359d9a1f67a49fe1d6a0447bf9f5e
lsm5-neurobots <lucas.mendonca@neurobots.com.br>
2021-03-23 22:42:41 -0300

    desafio concluido

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -60,7 +60,10 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
+
+      <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
+
+      {/*<nav className="sidebar">
         <span>Watch<p>Me</p></span>
 
         <div className="buttons-container">
@@ -75,9 +78,11 @@ export function App() {
           ))}
         </div>
 
-      </nav>
+          </nav>*/}
+        
+      <Content movies={movies} selectedGenre={selectedGenre} />
 
-      <div className="container">
+      {/*<div className="container">
         <header>
           <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
         </header>
@@ -89,7 +94,7 @@ export function App() {
             ))}
           </div>
         </main>
-      </div>
+            </div>*/}
     </div>
   )
 }
